9919f7877ce69d2eb097b49b9e0fde291f3b8405
Irakli Gozalishvili <rfobic@gmail.com>
2013-01-02 22:46:55 -0800

    Write docs.

--- difference.js
+++ difference.js
@@ -4,9 +4,26 @@ var slicer = Array.prototype.slice
 var concater = Array.prototype.concat
 var excludes = function excludes(item) { return this.indexOf(item) < 0 }
 
-module.exports = function difference(source, exclude, rest) {
-  if (!source) throw TypeError("difference requires  at least one arguments")
-  if (!exclude) return source
-  exclude = concater.apply([], slicer.call(arguments, 1))
-  return source.filter(excludes, exclude)
+module.exports = function difference(first, second) {
+  /**
+  Return a set that is the `first` set without elements of the remaining sets
+
+      var difference = require("interset/difference")
+
+      difference()
+      // => TypeError: difference requires at least one arguments
+
+      difference([1, 2, 3])
+      // => [1, 2, 3]
+
+      difference([1, 2], [2, 3])
+      // => [1]
+
+      difference([1, 2, 3], [1], [1, 4], [3])
+      // => [2]
+  **/
+  if (!first) throw TypeError("difference requires at least one argument")
+  if (!second) return first
+  var remaining = concater.apply([], slicer.call(arguments, 1))
+  return first.filter(excludes, remaining)
 }
